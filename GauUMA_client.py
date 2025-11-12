67cc3879dbc191888e085eada9f601e9f0146f5f
zhouoh <zhouqingyang139@gmail.com>
2025-11-12 12:59:32 -0800

    initial commit

---
+++ GauUMA_client.py
@@ -0,0 +1,141 @@
+import socket
+import struct
+import json
+import os
+import numpy as np
+import pickle
+import sys
+
+
+PORT = 50423
+
+PeriodicTable = ["H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"]
+
+bohr2angstrom = 0.529177249
+angstrom2bohr = 1.0/bohr2angstrom
+ev2hartree = 0.036749322
+
+
+def send_calculation_job(sock, xyz_content, charge, multiplicity, calc_type):
+
+    metadata = {
+        "filename": "mol", "charge": charge,
+        "multiplicity": multiplicity, "calc_type": calc_type,
+    }
+    header_bytes = json.dumps(metadata).encode('utf-8')
+    payload_bytes = xyz_content.encode('utf-8')
+    sock.sendall(struct.pack('Q', len(header_bytes)))
+    sock.sendall(header_bytes)
+    sock.sendall(struct.pack('Q', len(payload_bytes)))
+    sock.sendall(payload_bytes)
+    print("Sent job to server.")
+    return True
+
+
+def recvall(sock, n):
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet: return None
+        data.extend(packet)
+    return data
+
+def recv_object(sock): 
+    raw_msglen = recvall(sock, 8)
+    if not raw_msglen: return None
+    msglen = struct.unpack('Q', raw_msglen)[0]
+    
+    data = recvall(sock, msglen)
+    obj = pickle.loads(data)
+    return obj
+
+def save_result(result, output_file, calctype):
+    if calctype == 0:  # energy
+        with open(output_file, 'w') as f:
+            energy = result["energy"]
+            #energy, dipole-moment (xyz)	    	E, Dip(I), I=1,3	    	4D20.12
+            f.write(f"{energy:20.12e}{0:20.12e}{0:20.12e}{0:20.12e}\n")
+    elif calctype == 1:  # force
+        with open(output_file, 'w') as f:
+            energy = result["energy"]
+            forces = result["forces"]
+            #energy, dipole-moment (xyz)	    	E, Dip(I), I=1,3	    	4D20.12
+            f.write(f"{energy:20.12e}{0:20.12e}{0:20.12e}{0:20.12e}\n") 
+            #gradient on atom (xyz)	    	FX(J,I), J=1,3; I=1,NAtoms	    	3D20.12
+            for i in range(len(forces)):
+                f.write(f"{forces[i,0]:20.12e}{forces[i,1]:20.12e}{forces[i,2]:20.12e}\n".replace('e', 'E'))
+    elif calctype == 2:  # hessian
+        with open(output_file, 'w') as f:
+            energy = result["energy"]
+            forces = result["forces"] 
+            hessian = result["hessian"]
+            #energy, dipole-moment (xyz)	    	E, Dip(I), I=1,3	    	4D20.12
+            f.write(f"{energy:20.12e}{0:20.12e}{0:20.12e}{0:20.12e}\n") 
+            #gradient on atom (xyz)	    	FX(J,I), J=1,3; I=1,NAtoms	    	3D20.12
+            for i in range(len(forces)):
+                f.write(f"{forces[i,0]:20.12e}{forces[i,1]:20.12e}{forces[i,2]:20.12e}\n".replace('e', 'E'))
+            #polarizability		Polar(I), I=1,6		3D20.12
+            for i in range(2):
+                f.write(f"{0:20.12e}{0:20.12e}{0:20.12e}\n".replace('e', 'E'))
+            #dipole derivatives		DDip(I), I=1,9*NAtoms		3D20.12
+            for i in range(3*len(forces)):
+                f.write(f"{0:20.12e}{0:20.12e}{0:20.12e}\n".replace('e', 'E'))
+            #force constants		FFX(I), I=1,(3*NAtoms*(3*NAtoms+1))/2		3D20.12
+            #the Hessian is given in lower triangular form: αij, i=1 to N, j=1 to i. 
+            num_coords = hessian.shape[0]
+            triangle_indices = np.tril_indices(num_coords)
+            hessian_lower = hessian[triangle_indices]
+            for i in range(0, len(hessian_lower), 3):
+                row = hessian_lower[i:i+3]
+                while len(row) < 3:
+                    row = np.append(row, 0.0)
+                f.write(f"{row[0]:20.12e}{row[1]:20.12e}{row[2]:20.12e}\n".replace('e', 'E'))
+                #f.write(f"{1:20.12e}{1:20.12e}{1:20.12e}\n".replace('e', 'D'))
+
+# --- 主函数 ---
+def main():
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    calctyp_dict = {0: "energy", 1: "force", 2: "hessian"}
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+        NUM_ATOM = int(lines[0].split()[0])
+        calctype = int(lines[0].split()[1])
+        calctype_str = calctyp_dict.get(calctype, "energy")
+        charge = int(lines[0].split()[2])
+        multiplicity = int(lines[0].split()[3])
+        Geom = np.zeros((NUM_ATOM, 3))
+        Elements = []
+        for i in range(1, NUM_ATOM + 1):
+            line = lines[i].split()
+            Elements.append(PeriodicTable[int(line[0]) - 1])
+            for j in range(1, 4):
+                Geom[i-1, j-1] = float(line[j])
+
+    xyz_content = f"{NUM_ATOM}\nGenerated by client\n"
+    for i in range(NUM_ATOM):
+        xyz_content += f"{Elements[i]} {Geom[i,0]*bohr2angstrom} {Geom[i,1]*bohr2angstrom} {Geom[i,2]*bohr2angstrom}\n"
+    
+    server_address = ('localhost', PORT)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect(server_address)
+        
+        if send_calculation_job(sock, xyz_content, charge, multiplicity, calctype_str):
+            result = recv_object(sock)
+            if result is None:
+                return
+        else:
+            return
+        print("Received result from server:")
+        print(f"SCF Done:  E(UMA) =  {result['energy']}     A.U.")
+        #close the connection
+        sock.close()
+
+    save_result(result, output_file, calctype)
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ GauUMA_server.py
@@ -0,0 +1,177 @@
+from ase.build import molecule
+import ase
+from fairchem.core import pretrained_mlip, FAIRChemCalculator
+import numpy as np
+import pickle
+import socket
+import struct
+import io
+import json
+import logging
+from fairchem.core.units.mlip_unit.api.inference import InferenceSettings
+
+
+MODEL_NAME = "uma-s-1p1"
+PORT = 50423
+BATCH_size = 8
+
+bohr2angstrom = 0.529177249
+angstrom2bohr = 1.0/bohr2angstrom
+ev2hartree = 0.036749322
+
+def send_object(sock, obj): 
+    data = pickle.dumps(obj)
+    message_header = struct.pack('Q', len(data))
+    sock.sendall(message_header)
+    sock.sendall(data)
+    logging.info("Sent result to client.")
+
+def recvall(sock, n):
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+def receive_calculation_job(sock):
+    # 接收元数据帧
+    header_len_bytes = recvall(sock, 8)
+    if not header_len_bytes: return None, None
+    header_len = struct.unpack('Q', header_len_bytes)[0]
+    header_bytes = recvall(sock, header_len)
+    if not header_bytes: return None, None
+    metadata = json.loads(header_bytes.decode('utf-8'))
+    
+    # 接收数据帧
+    payload_len_bytes = recvall(sock, 8)
+    if not payload_len_bytes: return None, None
+    payload_len = struct.unpack('Q', payload_len_bytes)[0]
+    payload_bytes = recvall(sock, payload_len)
+    if not payload_bytes: return None, None
+    xyz_content = payload_bytes.decode('utf-8')
+    
+    return metadata, xyz_content
+
+def Energy(atoms):
+    """
+    计算分子的能量和力。
+    """
+    energy = atoms.get_potential_energy() * ev2hartree
+    return energy
+
+def Force(atoms):
+    """
+    计算分子的力。
+    """
+    forces = -1 * atoms.get_forces() * ev2hartree / angstrom2bohr
+    return forces
+
+def get_hessian_numeric(atoms, h = 1e-3) -> np.ndarray:
+
+    num_atoms = atoms.get_number_of_atoms()
+    original_positions = atoms.get_positions()
+    print(original_positions)
+    hessian_matrix = np.zeros((len(atoms) * 3, len(atoms) * 3))
+    for i in range(num_atoms):
+        for j in range(3):
+
+            positions_plus = original_positions.copy()
+            positions_plus[i, j] += h
+            atoms.set_positions(positions_plus)
+            force_plus = atoms.get_forces()
+
+            positions_minus = original_positions.copy()
+            positions_minus[i, j] -= h
+            atoms.set_positions(positions_minus)
+            force_minus = atoms.get_forces()
+            
+            hessian_column = (force_plus.flatten() - force_minus.flatten()) / (2 * h)
+            hessian_matrix[:, i * 3 + j] = -hessian_column 
+
+    atoms.set_positions(original_positions)
+    
+    return hessian_matrix
+
+
+def Hessian(atoms) -> np.ndarray:
+    """
+    计算分子的Hessian矩阵。
+    """
+    hessian = get_hessian_numeric(atoms, h=1e-3)
+    hessian =  hessian * ev2hartree / (angstrom2bohr**2)
+    return hessian
+
+# --- 主函数 ---
+def main():
+    settings = InferenceSettings(
+    tf32=False,
+    activation_checkpointing=False,
+    merge_mole=False,
+    external_graph_gen=False,
+    internal_graph_gen_version=2,
+    )
+    predictor = pretrained_mlip.get_predict_unit(MODEL_NAME, device="cuda",inference_settings=settings)
+    mol = molecule("H2O")
+    calc = FAIRChemCalculator(predictor, task_name="omol")
+    mol.set_calculator(calc)
+    mol.get_potential_energy()  # 预热模型
+
+    server_address = ('localhost', PORT)
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(server_address)
+        server_socket.listen(1)
+        logging.info(f"Listening on port {PORT}...")
+
+        while True:
+            conn, addr = server_socket.accept()
+            with conn:
+                logging.info(f"Received connection from {addr}")
+                
+                metadata, xyz_content = receive_calculation_job(conn)
+                
+                if metadata and xyz_content:
+                    logging.info(f"Received job with metadata: {metadata}")
+                    atoms = ase.io.read(io.StringIO(xyz_content), format='xyz')
+                    atoms.set_calculator(calc)
+                    charge = metadata.get('charge', 0)
+                    multiplicity = metadata.get('multiplicity', 1)
+                    atoms.info.update({'charge': charge, 'spin': multiplicity})
+                    atoms.set_pbc(False)
+                    
+                    calc_type = metadata.get('calc_type')
+                    result = {}
+                    if calc_type == 'energy':
+                        energy = Energy(atoms)
+                        result['energy'] = energy
+                        logging.info("Energy calculation completed.")
+                    elif calc_type == 'force':
+                        energy = Energy(atoms)
+                        result['energy'] = energy
+                        forces = Force(atoms)
+                        result['forces'] = forces
+                        logging.info("Force calculation completed.")
+                    elif calc_type == 'hessian':
+                        energy = Energy(atoms)
+                        result['energy'] = energy
+                        forces = Force(atoms)
+                        result['forces'] = forces
+                        hessian = Hessian(atoms)
+                        result['hessian'] = hessian
+                        logging.info("Hessian calculation completed.")
+                    else:
+                        logging.warning(f"Unknown calculation type: {calc_type}")
+                        continue
+                
+                send_object(conn, result)
+
+if __name__ == "__main__":
+    main()
+
+
+                
+
+
